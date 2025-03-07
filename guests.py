7664cc4e5ca904c6f0d9c869a8cef92bde08993c
Yonas21 <yonalem21@gmail.com>
2025-03-08 00:29:40 +0300

    remove print guest to make the execution a abit faster

--- guests.py
+++ guests.py
@@ -1,5 +1,5 @@
 import requests
-from config import API_URL, AGENCY_UID
+from config import API_URL, AGENCY_UID, LEAD_UID
 from headers import headers
 
 def get_guests(AGENCY_URL, headers, AGENCY_UID):
@@ -7,5 +7,13 @@ def get_guests(AGENCY_URL, headers, AGENCY_UID):
     return response.json()
 
 
-guests_information = get_guests(API_URL, headers, AGENCY_UID)
-print(guests_information)
+def get_extra_guests(api_url, headers, lead_uid):
+    response = requests.get(f"{api_url}/extra-guests/{lead_uid}", headers=headers)
+    return response.json()
+
+# guests_information = get_guests(API_URL, headers, AGENCY_UID)
+# print(guests_information)
+
+
+extra_guests = get_extra_guests(API_URL, headers, LEAD_UID)
+print("extra_guests: ", extra_guests)
